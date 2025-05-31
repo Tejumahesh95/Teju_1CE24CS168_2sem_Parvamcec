0ee804b275ac8ad3ab6e88b3b30edcb3c392dd45
Tejumahesh95 <teju76278@gmail.com>
2025-05-31 16:08:51 -0700

    message

---
+++ day10/2_bis_registration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string productName, manufacturerName;
+    cout << "Enter product name: ";
+    getline(cin, productName);
+
+    cout << "Enter manufacturer name: ";                                                                       er manufacturer name: ";
+    getline(cin, manufacturerName);
+
+    cout << "\nSimulating BIS Registration Process" << endl;
+    cout << "Product Name: " << productName << endl;
+    cout << "Manufacturer: " << manufacturerName << endl;
+    cout << "Registration Process Complete (Simulated)" << endl;
+    return 0;
+}
